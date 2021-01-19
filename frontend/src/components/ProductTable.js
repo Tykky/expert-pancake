59e0a7ecfa8ea15b61a24599f0d01964d38938d1
Tykky <40919314+Tykky@users.noreply.github.com>
2021-01-19 22:10:04 +0200

    Replace infinite scroll with react virtualized

--- frontend/src/components/ProductTable.js
+++ frontend/src/components/ProductTable.js
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import config from '../config'
-import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Paper } from '@material-ui/core'
+//import config from '../config'
+import { Table, /*TableHead, TableBody, TableRow,*/ TableCell } from '@material-ui/core'
+import { WindowScroller, Table as VirtualTable, Column, AutoSizer } from 'react-virtualized'
+import 'react-virtualized/styles.css'
 
 const ProductTable = (props) => {
-
-  const [ productDisplay, setProductDisplay ] = useState()
-
-  useEffect(() => {
-    if (props.products) {
-      setProductDisplay(
-        props.products.length > config.infinite_scroll_default ?
-          props.products.slice(1, config.infinite_scroll_default) :
-          [...props.products]
-      )
-    }
-  },[props.products])
 
   const instockValue = {
     INSTOCK: <b style={{ color: 'green' }}>in stock</b>,
@@ -24,47 +13,112 @@ const ProductTable = (props) => {
     LESSTHAN10: <b style={{ color: 'orange' }}>less than 10</b>
   }
 
+  const cellRenderer = ({ cellData }) => (
+    <TableCell
+      component='div'
+      variant='body'
+    >
+      {cellData}
+    </TableCell>
+  )
+
+  const headerRenderer = (label) => (
+    <TableCell
+      component='div'
+      variant='head'
+    >
+      {label.toUpperCase()}
+    </TableCell>
+  )
+
+  const columns = [
+    {
+      width: 220,
+      label: 'Id',
+      'dataKey': 'id',
+      cell: cellRenderer,
+      head: () => headerRenderer('Id')
+    },
+    {
+      width: 250,
+      label: 'Name',
+      dataKey: 'name',
+      cell: cellRenderer,
+      head: () => headerRenderer('Name')
+    },
+    {
+      width: 100,
+      label: 'Colors',
+      dataKey: 'color',
+      cell: ({ cellData }) => cellData.join(', '),
+      head: () => headerRenderer('Colors')
+    },
+    {
+      width: 100,
+      label: 'Price',
+      dataKey: 'price',
+      cell: cellRenderer,
+      head: () => headerRenderer('Price')
+    },
+    {
+      width: 150,
+      label: 'Manufacturer',
+      dataKey: 'manufacturer',
+      cell: cellRenderer,
+      head: () => headerRenderer('Manufacturer')
+    },
+    {
+      width: 100,
+      label: 'Availability',
+      dataKey: 'manufacturer',
+      cell: ({ cellData, rowIndex }) => {
+        const product = props.products[rowIndex]
+        let avail
+        if (props.availability[cellData]) {
+          avail = props.availability[cellData][product.id]
+        }
+        return avail ? instockValue[avail] : 'fetching...'
+      },
+      head: () => headerRenderer('Availability')
+    }
+  ]
+
   return (
-    productDisplay ?
-      <InfiniteScroll
-        dataLength={productDisplay.length}
-        next={() => setProductDisplay(ProductDisplay => (
-          props.products.slice(1, ProductDisplay.length + config.infinite_scroll_increment)
-        ))}
-        hasMore={props.products.length > setProductDisplay.length}
-      >
-        <TableContainer component={Paper} variant='outlined'>
-          <Table size="small" stickyHeader={true}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Name</TableCell>
-                <TableCell>Colors</TableCell>
-                <TableCell>Price</TableCell>
-                <TableCell>Manufacturer</TableCell>
-                <TableCell>Availability</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {productDisplay.map(product => (
-                <TableRow key={ product.id }>
-                  <TableCell>{ product.name }</TableCell>
-                  <TableCell>{ product.color.join(', ') }</TableCell>
-                  <TableCell>{ product.price }</TableCell>
-                  <TableCell>{ product.manufacturer }</TableCell>
-                  <TableCell>
-                    {
-                      props.availability[product.manufacturer][product.id] ?
-                        instockValue[props.availability[product.manufacturer][product.id]] :
-                        'fetching ...'
-                    }
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </InfiniteScroll>
-      : <p>Loading...</p>
+    props.products ?
+      <Table size='small'>
+        <WindowScroller>
+          {({ height, isScrolling, onChildScroll, scrollTop }) => (
+            <AutoSizer>
+              {({ width }) => (
+                <VirtualTable
+                  autoHeight
+                  height={height}
+                  isScrolling={isScrolling}
+                  onScroll={onChildScroll}
+                  rowCount={props.products.length}
+                  rowHeight={20}
+                  headerHeight={20}
+                  scrollTop={scrollTop}
+                  width={width}
+                  rowGetter={({ index }) => props.products[index]}
+                >
+                  {columns.map(col => (
+                    <Column
+                      key={col.dataKey}
+                      label={col.label}
+                      dataKey={col.dataKey}
+                      headerRenderer={col.head}
+                      cellRenderer={col.cell}
+                      width={col.width}
+                    />
+                  ))}
+                </VirtualTable>
+              )}
+            </AutoSizer>
+          )}
+        </WindowScroller>
+      </Table>
+      : <p>loading...</p>
   )
 }
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -13,8 +13,6 @@ const config = {
       name: 'beanies',
       id: 2
     }],
-  infinite_scroll_increment: 400,
-  infinite_scroll_default: 200
 }
 
 export default config
